2404e69c2f743855a98ba04149cb7e24779799d3
Pavel <naxa@nextmail.ru>
2021-03-28 19:37:48 +0200

    feat: solve Conditions'n'Loops tasks (draft)

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -227,8 +227,11 @@ function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
 }
 
 
@@ -244,8 +247,8 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  return +reverseString(num.toString());
 }
 
 
@@ -255,7 +258,7 @@ function reverseInteger(/* num */) {
  *
  * See algorithm here : https://en.wikipedia.org/wiki/Luhn_algorithm
  *
- * @param {number} cnn
+ * @param {number} ccn
  * @return {boolean}
  *
  * @example:
@@ -269,8 +272,13 @@ function reverseInteger(/* num */) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const sumOfDigits = ccn.toString()
+    .split('')
+    .reverse()
+    .map((x, i) => (i % 2 ? x * 2 : +x))
+    .reduce((sum, it) => sum + (it > 9 ? it - 9 : it), 0);
+  return (sumOfDigits * 9) % 10 === 0;
 }
 
 /**
